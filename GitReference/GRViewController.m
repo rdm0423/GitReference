27597a91e7cbfa11f3ae70f91393b5130a403d23
Ross McIlwaine <rdm0423@gmail.com>
2015-02-02 18:55:57 -0700

    Step 2

--- GitReference/GRViewController.m
+++ GitReference/GRViewController.m
@@ -8,6 +8,10 @@
 
 #import "GRViewController.h"
 
+static CGFloat margin = 15;
+static NSString * const Command = @"command";
+static NSString * const Reference = @"reference";
+
 @interface GRViewController ()
 
 @end
@@ -17,7 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    CGFloat topMagin = 20;
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, topMagin, self.view.frame.size.width, self.view.frame.size.height)];
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width + 10, self.view.frame.size.height + 10);
+    [self.view addSubview:scrollView];
+    
+    CGFloat x = CGRectGetMaxX(self.view.frame);
+    
+    UILabel *gitReference = [[UILabel alloc] initWithFrame:CGRectMake(100, 64, x, 20)];
+    gitReference.text = @"Git Reference";
+    [scrollView addSubview:gitReference];
+                             
 }
+
+- (NSArray *)gitCommands {
+    
+    return @[@{Command: @"git status", Reference: @": shows changed files"},
+             @{Command: @"git diff", Reference: @": shows actual changes"},
+             @{Command: @"git add .", Reference: @": adds changed files to the commit"},
+             @{Command: @"git commit -m \"notes\"", Reference: @": commits the changes"},
+             @{Command: @"git push origin _branch_", Reference: @": pushes commits to branch named _branch_"},
+             @{Command: @"git log", Reference: @": displays progress log"},
+             @{Command: @"git comment --amend", Reference: @": re-enter last commit message"}
+             ];
+    
+}
+
+- (CGFloat)heightOfReferenceString:(NSString *)reference {
+    
+    CGRect bounding = [reference boundingRectWithSize:CGSizeMake(self.view.frame.size.width - 2 * margin, 0)
+                                              options:NSStringDrawingUsesLineFragmentOrigin
+                                           attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15]}
+                                              context:nil];
+    
+    return bounding.size.height;
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
